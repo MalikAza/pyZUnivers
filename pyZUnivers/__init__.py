25b29af441da7eb0c2fd5aee65341e057bbbd7f6
Dakota <dakota.froissart@letsco.co>
2023-06-26 20:26:36 +0200

    feat: get reputation in module

--- pyZUnivers/__init__.py
+++ pyZUnivers/__init__.py
@@ -2,6 +2,7 @@ from .user import User
 from .overview import UserOverview
 from .loot_infos import UserLootInfos
 from .challenges import Challenges
+from .reputation import UserReputation
 
 def get_user_overview(username: str) -> UserOverview:
     return UserOverview(username)
@@ -11,3 +12,6 @@ def get_user_loot_infos(username: str) -> UserLootInfos:
 
 def get_challenges(username: str = None) -> Challenges:
     return Challenges(username)
+
+def get_user_reputations(username: str) -> UserReputation:
+    return UserReputation(username)
