1f8d6f3ec24236c1ed418eb6d66cbc385e81f08b
Dakota <dakota.froissart@letsco.co>
2023-06-26 20:22:34 +0200

    feat: reputation (+restructuring of tests)

---
+++ pyZUnivers/reputation.py
@@ -0,0 +1,50 @@
+import urllib.parse
+from .utils import (
+    API_BASE_URL,
+    get_datas
+)
+
+class _ReputationClan:
+
+    def __init__(self, payload) -> None:
+        self.__payload = payload
+        self.__reputation_level = payload['reputationLevel']
+
+    @property
+    def name(self) -> str:
+        return self.__payload['reputation']['name']
+    
+    @property
+    def level_name(self) -> str:
+        return self.__reputation_level['name']
+    
+    @property
+    def progress(self) -> str:
+        return f"{self.__payload['value']}/{self.__reputation_level['toValue'] + 1}"
+
+class UserReputation:
+
+    def __init__(self, username: str) -> None:
+        self.name = username.replace('#0', '') if username.endswith('#0') else username
+        self.__parsed_name = urllib.parse.quote(self.name)
+        self.__infos = get_datas(f"{API_BASE_URL}/tower/{self.__parsed_name}")['reputations']
+
+    @property
+    def first(self) -> _ReputationClan:
+        return _ReputationClan(self.__infos[0])
+    
+    @property
+    def second(self) -> _ReputationClan:
+        return _ReputationClan(self.__infos[1])
+    
+    @property
+    def third(self) -> _ReputationClan:
+        return _ReputationClan(self.__infos[2])
+    
+    @property
+    def fourth(self) -> _ReputationClan:
+        return _ReputationClan(self.__infos[3])
+    
+    @property
+    def fifth(self) -> _ReputationClan:
+        return _ReputationClan(self.__infos[4])
---
+++ tests/README.md
@@ -0,0 +1,14 @@
+# Running test
+## All tests
+In project root:
+
+```bash
+python3 -m unittest discover -v tests/
+```
+
+## Specific tests
+In project root:
+
+```bash
+python3 -m unittest tests/<file_name>
+```
