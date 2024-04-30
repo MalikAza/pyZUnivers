615312aaef6fed7ef4461af0cbfbc9200e732b9e
Dakota <dakota742009@gmail.com>
2024-04-30 14:08:23 +0200

    feat: #8 added ZUnivers channels ID from Zerator's Discord Guild

--- pyZUnivers/__init__.py
+++ pyZUnivers/__init__.py
@@ -7,4 +7,10 @@ from .vortex import Vortex
 from .events import Events
 from .achievements import Achievements
 from .tickets import AutoGratting
-from .utils import ZUniversAPIError, DISCORD_DATE_FORMAT, is_advent_calendar, get_ascension_leaderboard, get_inventory
+from .utils import (
+    ZUniversAPIError,
+    DISCORD_DATE_FORMAT,
+    is_advent_calendar,
+    get_ascension_leaderboard,
+    get_inventory
+)
