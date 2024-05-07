7f2dfe0fd6666255888d10d2b984e442ea1225c3
Dakota <dakota742009@gmail.com>
2024-05-08 00:08:14 +0200

    because of ZUDevs, checks format for subscription dates

--- pyZUnivers/subscription.py
+++ pyZUnivers/subscription.py
@@ -4,5 +4,8 @@ from datetime import datetime
 class Subscription:
 
     def __init__(self, payload) -> None:
-        self.begin = datetime.strptime(payload["beginDate"], FULL_DATE_TIME_FORMAT)
-        self.end = datetime.strptime(payload["endDate"], FULL_DATE_TIME_FORMAT)
+        # checks if payload beginDate and endDate are in format %Y-%m-%dT%H:%M:%S.%f or %Y-%m-%dT%H:%M:%S
+        begin_date_format = FULL_DATE_TIME_FORMAT if "." in payload["beginDate"] else DATE_TIME_FORMAT
+        end_date_format = FULL_DATE_TIME_FORMAT if "." in payload["endDate"] else DATE_TIME_FORMAT
+        self.begin = datetime.strptime(payload["beginDate"], begin_date_format)
+        self.end = datetime.strptime(payload["endDate"], end_date_format)
