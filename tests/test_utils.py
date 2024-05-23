dc65e3b17f746f89bedfba4af35eb45b30bf446c
Dakota <dakota742009@gmail.com>
2024-05-23 16:03:02 +0200

    tests: utils post_datas

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -33,3 +33,34 @@ class GetDatasTest(unittest.TestCase):
         url = "https://example.com/api"
         with self.assertRaises(Exception):
             utils.get_datas(url)
+
+class PostDatasTest(unittest.TestCase):
+
+    @patch('pyZUnivers.utils.requests.post')
+    def test_post_datas_success(self, mock_post):
+        mock_response = {
+            "key1": "value1",
+            "key2": "value2"
+        }
+        mock_post.return_value.__enter__.return_value.json.return_value = mock_response
+
+        url = "https://example.com/api"
+        result = utils.post_datas(url)
+
+        self.assertEqual(result, mock_response)
+
+    @patch('pyZUnivers.utils.requests.post')
+    def test_post_datas_json_decode_error(self, mock_post):
+        mock_post.return_value.__enter__.return_value.json.side_effect = JSONDecodeError("", "", 0)
+
+        url = "https://example.com/api"
+        with self.assertRaises(utils.ZUniversAPIError):
+            utils.post_datas(url)
+
+    @patch('pyZUnivers.utils.requests.post')
+    def test_post_datas_other_exception(self, mock_post):
+        mock_post.return_value.__enter__.return_value.json.side_effect = Exception
+
+        url = "https://example.com/api"
+        with self.assertRaises(Exception):
+            utils.post_datas(url)
