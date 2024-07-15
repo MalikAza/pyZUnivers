a072b35702e555d0f9fb62352fef65854edb6a05
Dakota <dakota742009@gmail.com>
2024-07-15 14:33:42 +0200

    fix: towerStat key not always present

--- pyZUnivers/overview.py
+++ pyZUnivers/overview.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from .api_responses import Overview
 from .pins import UserPin
+from .vortex import Vortex
 from .utils import (
     get_datas,
     API_BASE_URL,
@@ -29,7 +30,7 @@ class UserOverview:
         self.name, self.__parsed_name = parse_username(username)
         self.__infos: Overview = get_datas(f"{API_BASE_URL}/user/{self.__parsed_name}/overview")
         self.__pins = self.__infos['pins']
-        self.__vortex_stats = self.__infos['towerStat']
+        self.__vortex_stats = self.__infos['towerStat'] if 'towerStat' in self.__infos else Vortex() # Vortex stats are not always available.
 
     @property
     def pins(self) -> List[UserPin]:
@@ -41,26 +42,38 @@ class UserOverview:
     
     @property
     def vortex_name(self) -> str:
+        if self.__vortex_stats == Vortex:
+            return self.__vortex_stats.name
+        
         return self.__vortex_stats['towerName']
     
     @property
     def vortex_begin_date(self) -> datetime:
+        if self.__vortex_stats == Vortex:
+            return self.__vortex_stats.begin_date
+
         return datetime.strptime(self.__vortex_stats['towerSeasonBeginDate'], DATE_FORMAT)
 
     @property
     def vortex_end_date(self) -> datetime:
+        if self.__vortex_stats == Vortex:
+            return self.__vortex_stats.end_date
+
         return datetime.strptime(self.__vortex_stats['towerSeasonEndDate'], DATE_FORMAT)
 
     @property
     def vortex_floor(self) -> int:
-        if not self.__vortex_stats or not self.__vortex_stats['maxFloorIndex']:
+        if self.__vortex_stats == Vortex or not self.__vortex_stats['maxFloorIndex']:
             return 0
+        
         if self.__vortex_stats['maxFloorIndex'] == 0:
             return 1
+        
         return self.__vortex_stats['maxFloorIndex'] + 1
     
     @property
     def vortex_tries(self) -> int:
-        if not self.__vortex_stats:
+        if self.__vortex_stats == Vortex:
             return 0
+        
         return self.__vortex_stats['towerLogCount']
